fde3933da98d2790c569e67fae46639a970f4f41
Xinglun Xu <xx688@nyu.edu>
2018-04-07 15:42:01 -0400

    js library and login page

---
+++ static/view/js/login.js
@@ -0,0 +1,26 @@
+var url = "http://localhost:3000";
+
+$( "#btnReg" ).click(function() {
+  $.get( url+"/User/Register",{userName:$("#userName").val() ,password:$("#password").val()}).done(function( data ) {
+      console.log(data);
+    	if(data=="0") alert("User Name is occupied, try another one.");
+    	else{
+    		console.log("Start replace");
+    		window.location.replace(url);
+    		console.log("End replace");
+    	}
+  	});
+}); 
+
+$("#btnLogin").click(function(){
+	$.get( url+"/User/Login",{userName:$("#userName").val() ,password:$("#password").val()}).done(function( data ) {
+      console.log("yaayay");
+      var loginSuccess = data==true;
+  		if(loginSuccess){
+  			window.location.replace(url);
+  		}
+  		else{
+  			alert("Either the Username or the password is incorrect");
+  		}
+  	});
+});
