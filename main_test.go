c7284fad1e0c4a5d55a870db57a2cd6496d5e0e8
Xinglun Xu <xx688@nyu.edu>
2018-04-08 23:11:29 -0400

    write a httptest

--- main_test.go
+++ main_test.go
@@ -2,44 +2,48 @@ package main
 
 import (
   "testing"
-  "log"
+  // "log"
   "fmt"
+  "github.com/gorilla/sessions"
+  "net/http/httptest"
+  "io/ioutil"
+  // "strings"
+  "net/url"
 )
-// msg = {
-// 			  {
-//           ID  : 0,
-//           Value : "I like debuging :)",
-//           User  : "usera",
-//           LikeNum  : 2,
-//           IsLiked : false,
-// 			  },
-//         {
-//           ID  : 1,
-//           Value : "I literally like debuging :)",
-//           User  : "userb",
-//           LikeNum  : 3,
-//           IsLiked : false,
-// 			  },
-//         {
-//           ID  : 2,
-//           Value : "I really like debuging :)",
-//           User  : "userc",
-//           LikeNum  : 3,
-//           IsLiked : false,
-// 			  },
-//  			}
 
-func TestLogin(t *testing.T){
-  // var user map[string]User
+// func TestLogin(t *testing.T){
+//   // var user map[string]User
+//   user = make(map[string]User)
+//   var temp = User{"name", "password"}
+//   user["name"] = temp
+//   log.Println(user)
+//   ret := _login("name", "password")
+//   if ret != "true"{
+//     t.Fatalf("TestLogin fail")
+//   }
+//   fmt.Printf("Login Passed\n")
+// }
+
+
+func TestLogin2(t *testing.T){
+  store = sessions.NewCookieStore([]byte("something-very-secret"))
   user = make(map[string]User)
-  var temp = User{"name", "password"}
-  user["name"] = temp
-  log.Println(user)
-  ret := _login("name", "password")
-  if ret != "true"{
-    t.Fatalf("TestLogin fail")
+  user["user"] = User{"user", "password"}
+  data := url.Values{}
+  data.Set("user", "user")
+  data.Add("password", "password")
+
+  r := httptest.NewRequest("GET", "/User/Login?"+data.Encode(), nil)
+  w := httptest.NewRecorder()
+  login(w,r)
+
+  resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+  ret := string(body)
+  if ret=="false"{
+    t.Fatalf("TestLogin2 fail")
   }
-  fmt.Printf("Login Passed\n")
 }
 
 func TestisLike(t *testing.T) {
