7a9f8344554e01a8b234e7ade012549c033c3ec0
Xinglun Xu <xx688@nyu.edu>
2018-04-20 16:30:36 -0400

    add delete user and sendmsg test

--- src/db/main_test.go
+++ src/db/main_test.go
@@ -10,6 +10,15 @@ import (
   "strconv"
 )
 
+type Test_DB struct{
+  client *rpc.Client
+}
+
+func (test_db *Test_DB) signUpUser(uname string, password string){
+  args := &common.SignArgs{uname,password}
+  var reply common.SignReply
+  test_db.client.Call("DB.Signup", args, &reply)
+}
 
 func BuildSuiteWithPort(port int) (*DB, *rpc.Client){
   db := new(DB)
@@ -76,5 +85,46 @@ func Test_Login(t *testing.T){
   err2 := client.Call("DB.Login", args2, &reply2)
   if(reply2.Password!=passWord){
     log.Fatal("Test_Login: fail to login", err2)
+  }
+}
+
+
+
+
+func Test_DelUser(t *testing.T){
+  _,client := BuildSuiteWithPort(18083)
+  userName := "lala"
+  passWord := "weakPW"
+  args1 := &common.SignArgs{userName,passWord}
+  var reply1 common.SignReply
+  client.Call("DB.Signup", args1, &reply1)
+
+  args2 := &common.DelUserArgs{userName}
+  var reply2 common.DelUserReply
+  client.Call("DB.DelUser", args2, &reply2)
+
+  args3 := &common.LogArgs{userName}
+  var reply3 common.LogReply
+  client.Call("DB.Login", args3, &reply3)
+
+  if(reply3.Success==true){
+    log.Fatal("Test_DelUser: deleted user still able to login")
+  }
+}
+
+func (test_db *Test_DB) sendMsgFromUser(uname string, value string){
+  args := &common.SendMsgArgs{uname,value}
+  var reply common.SendMsgReply
+  test_db.client.Call("DB.SendMsg", args, &reply)
+}
+
+func Test_SendMsg(t *testing.T){
+  db,client := BuildSuiteWithPort(18084)
+  test_db := Test_DB{client}
+  test_db.signUpUser("u1", "p1")
+
+  test_db.sendMsgFromUser("u1", "value1")
+  if len(db.msg)<1 {
+    log.Fatal("Test_SendMsg: fail to insert message")
   }
 }
